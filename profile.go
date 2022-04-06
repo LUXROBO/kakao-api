244ed7f52910c237197cf7a9132288af07287e7c
csk6124 <csk6124@gmail.com>
2022-04-06 18:48:46 +0900

    feat: 프로필 데이타 추출수정

--- profile.go
+++ profile.go
@@ -8,14 +8,14 @@ import (
 )
 
 // GetProfileEmail 카카오 프로필
-func (c Client) GetProfileEmail(tokenStr string) (string, error) {
+func (c Client) GetProfileEmail(tokenStr string) (*schema.ProfileData, error) {
 	body, err := c.requestGetAPI(tokenStr, KakaoAPIPAth+"/v2/user/me")
 	if WorkCheck("requestAPI", err) != nil {
-		return "", err
+		return nil, err
 	}
 
 	profile := &schema.ProfileData{}
 	err = json.Unmarshal(body, &profile)
 	logger.Debug("profile", profile)
-	return profile.KakaoAccount.Email, err
+	return profile, err
 }
